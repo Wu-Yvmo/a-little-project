4e44799f8fe2b363305c4b517acf0801501bb0ae
Wu-Yvmo <1224114710@qq.com>
2021-05-15 01:26:42 +0800

    添加了一个查找某个值在树中是否存在的接口

--- 源代码/binary_search_tree.cpp
+++ 源代码/binary_search_tree.cpp
@@ -27,11 +27,16 @@ private:
     node* root;
     //binary_search_tree类私有，该方法删除参数指针指向的树
     void remove(node*);
+    //这是查找的私有接口，供公有实现调用
+    bool exists(object & x,node* t);
+    
 public:
     //构造函数
     binary_search_tree();
     //析构函数，调用了私有的remove(node* a)函数
     ~binary_search_tree();
+    //查找树中是否存在大小等于x的节点
+    bool exists(object & x)；
 };
 
 template<typename object>
@@ -57,4 +62,23 @@ template<typename object>
 binary_search_tree<object>::~binary_search_tree()
 {
     remove(root);
+} 
+
+template<typename object>
+bool binary_search_tree<object>::exists(object & x,node* t)
+{
+    if (t == nullptr)
+        return false;
+    else if (t->the_data > x)
+        return exists(object & x,t->left);
+    else if (t->the_data < x)
+        return exists(object & x,t->right)
+    else
+        return true;
+}
+
+template<typename object>
+bool binary_search_tree<object>::exists(object & x)
+{
+    return exists(object & x,root);
 }
