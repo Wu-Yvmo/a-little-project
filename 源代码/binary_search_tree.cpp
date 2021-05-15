186f6e62e88a15a8204f4fc688fa4c5ac89ea2f6
Wu-Yvmo <1224114710@qq.com>
2021-05-15 12:39:33 +0800

    添加了一个复制构造函数

--- 源代码/binary_search_tree.cpp
+++ 源代码/binary_search_tree.cpp
@@ -2,6 +2,7 @@ template<typename object>
 class binary_search_tree
 {
 private:
+
     //二叉树的节点由一个结构体实现
     //包括节点本身，指向左树的指针，指向右树的指针
     //我顺便写了两个构造函数，应该后面会有用
@@ -37,20 +38,27 @@ private:
     void get_in_private(object & x,node* t);
     //向树中删除一个节点，内部实现
     void remove_private(object & x,node* t);
+    //复制构造函数的内部接口
+    node* clone(node* t);
+
 public:
     //构造函数
     binary_search_tree();
+    //复制构造函数
+    binary_search_tree(binary_search_tree &);
     //析构函数，调用了私有的remove(node* a)函数
     ~binary_search_tree();
+
     //查找树中是否存在大小等于x的节点
     bool exists(object & x);
     //查找最小值，公有接口
     node* find_min();
     //查找最大值，公有接口
     node* find_max();
+
     //向树中插入一个元素
     void get_in(object & x);
-    //从树中删除一个节点
+    //从树中删除一个元素
     void remove(object & x);
 };
 
@@ -190,4 +198,23 @@ template <typename object>
 void binary_search_tree<object>::remove(object & x)
 {
     remove_private(x,root);
+}
+
+template<typename object>
+binary_search_tree<object>::node* binary_search_tree<object>::clone(node* t)
+{
+    if (t == nullptr)
+    {
+        return nullptr;
+    }
+    else
+    {
+        return new node(t->the_data,clone(t->left),clone(t->right));
+    }
+}
+
+template<typename object>
+binary_search_tree<object>::binary_search_tree(binary_search_tree & a)
+{
+    root=clone(a);
 }
